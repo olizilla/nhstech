a01a2ff2c323b82419ab4b14be4679b3d51bfbd0
Oli Evans <oli.evans@gmail.com>
2025-11-07 21:15:29 +0000

    chore: fix base for github pages

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -6,7 +6,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://nhstech.uk',
+  site: 'https://olizilla.github.io',
+  base: '/nhstech',
   vite: {
     plugins: [tailwindcss()]
   },
